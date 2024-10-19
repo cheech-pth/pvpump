c2cac3c20a7dd92bb978a07f3f4575b0c842128a
Cheech-pth <michael.gogarty@gmail.com>
2024-10-20 09:54:22 +1000

    Cleaned up app/page.js (Main root page) and removed monkey.png

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # PVPump Dashboard
 
-WIP NextJS project to create a dashboard for Pump.fun
+WIP NextJS educational project to create a dashboard for Pump.fun and learn new concepts.
+
+**It may or may not be accurate, don't use this as a tool for trading.**
+
+![Preview of the dashboard with trades populated](preview.png)
--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
 'use client'
 
+import Link from 'next/link';
+
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col justify-center bg-mars-black text-mars-orange">
-      <div>Hello world!</div>
-    </main>
+<main className="flex justify-center items-center h-screen bg-mars-black text-mars-orange">
+  <div className="max-w-md w-full px-4 py-8 md:px-6 lg:px-8 xl:px-10">
+    <Link href="/dashboard" passHref>
+      <h1 className="text-xl font-bold">Go to Dashboard</h1>
+    </Link>
+  </div>
+</main>
   );
 }
